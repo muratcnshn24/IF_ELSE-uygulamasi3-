c71aafbc224da4af1dfaa29a303f378930a856ee
muratcnshn24 <127681337+muratcnshn24@users.noreply.github.com>
2024-07-18 21:24:36 +0300

    Proje dosyası ekle.

---
+++ IF_ELSE(uygulamasi3)/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IF_ELSE_uygulamasi3_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int m = 9;
+            int n = 7;
+            int p = 5;
+
+            if (m>=n && m>p)
+            {
+                Console.WriteLine(" En Büyük m");
+            }
+
+            if (m>n && !(p>m))  // ünlemi gördüğünde true çevirecek
+            {
+                Console.WriteLine(" En Büyük m");
+            }
+
+            // || veya demek
+            if (m>n || m>p)
+            {
+                Console.WriteLine("m En Küçük Değil");
+            }
+
+            // burada else bloğu yok şart sağlarsa çıktıyı ekrana veriyor
+
+            m = 4;
+            if (!(m>=n || m>=p ))
+            {
+                Console.WriteLine("m Artık En Küçük");
+            }
+        }
+    }
+}
